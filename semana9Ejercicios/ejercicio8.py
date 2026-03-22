3cf48aabea8bc0f6411dd998d3178a9e35c773c3
barrerarobin43 <barrerarobin43@gmail.com>
2026-03-21 18:46:38 -0600

    en este ejercicio se pide una frase y se cuenta la cantidad de veces que aparece la leta A

---
+++ semana9Ejercicios/ejercicio8.py
@@ -0,0 +1,6 @@
+"""Cree un programa que solicite una frase y cuente cuántas veces aparece la letra "a" dentro de la frase.
+Puede utilizar el método count()."""
+
+frase = input("Ingrese una frase: ")
+contar = frase.count("a")
+print(contar)
