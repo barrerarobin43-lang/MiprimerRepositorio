d15e01aefdd749f93fc6235b025a2b9c59695e8e
barrerarobin43 <barrerarobin43@gmail.com>
2026-03-21 18:42:32 -0600

    en este ejercicio se pide una frase y solo se muestra la priemra y ultima letra

---
+++ semana9Ejercicios/ejercicio5.py
@@ -0,0 +1,6 @@
+"Solicite al usuario una frase y muestre la primera y la última letra de la frase."
+frase = input("Ingrese una frase: ")
+firt = frase[0]
+second = frase[-1]
+print("La primera letra es:", firt)
+print("La ultima letra es:", second)
